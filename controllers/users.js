80c8f0f3b140a3d57b923ac24f07f9d22ef8bde3
Alexandr Ezhuchenkov <ezhuchenkov@gmail.com>
2019-10-19 23:27:57 +0300

    в контроллере createUser почта и хеш пароля записываются в базу, установлен модуль bcryptjs

--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,42 @@
+import { find, findById } from '../models/user'
+
+// eslint-disable-next-line import/no-unresolved
+const bcrypt = require('bcryptjs')
 const User = require('../models/user')
+
 
 const errorMessage = { message: 'Произошла ошибка' }
 
-module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body
+export function createUser(req, res) {
+  const {
+    name, about, avatar, email, password,
+  } = req.body
 
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send(errorMessage))
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
+    .then((user) => {
+      res.status(201).send({
+        _id: user._id, name: user.name, about: user.about, email: user.email,
+      })
+    })
+    .catch(() => {
+      res.status(400).send(errorMessage)
+    })
 }
 
-
-module.exports.getAllUsers = (req, res) => {
-  User.find({})
+export function getAllUsers(req, res) {
+  find({})
     .then((users) => res.send({ data: users }))
     .catch(() => res.status(500).send(errorMessage))
 }
-module.exports.getUserById = (req, res) => {
-  User.findById(req.params.id)
+export function getUserById(req, res) {
+  findById(req.params.id)
     .then((user) => res.send({ data: user }))
     .catch(() => res.status(500).send(errorMessage))
 }
