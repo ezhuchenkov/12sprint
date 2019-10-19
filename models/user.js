b7e2d9e6df888b7249059b986a5a331fed23e4ca
Alexandr Ezhuchenkov <ezhuchenkov@gmail.com>
2019-10-19 20:56:30 +0300

    установлен пакет validator, в схеме пользователя добавлены обязательные email и password, поле email уникально и валидируется

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validate = require('validator')
 // eslint-disable-next-line no-useless-escape
 const urlRegExp = [/^((http|https)):\/\/(www\.)?((\d{3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(:\d{1,5})?)|([A-z]+(\.[\w-]+)?\.[A-z]{2,4}))(\/[\w-\/]+)?#?/]
 
@@ -19,6 +20,20 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     match: urlRegExp,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => validate.isEmail(v),
+      message: 'Введенное значение не вляется email',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
   },
 })
 
