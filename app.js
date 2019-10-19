5298263d78322d5262aa97c948407386882c7f16
Alexandr Ezhuchenkov <ezhuchenkov@gmail.com>
2019-10-20 00:18:28 +0300

    создан мидлвар auth, добавляющий в объект запроса payload, защищены авторизацией все маршруты, кроме создания нового пользователя и логина, удалён хардкод с id

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const usersRoute = require('./routes/users')
 const cardsRoute = require('./routes/cards')
 const { createUser, login } = require('./controllers/users')
+const auth = require('./middlewares/auth')
 
 const { PORT = 3000 } = process.env
 const app = express()
@@ -10,28 +11,19 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5d9dbaa2b4c7ac154caeaf12',
-  }
-
-  next()
-})
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 })
-
-
+app.post('/signin', login)
+app.post('/signup', createUser)
+app.use(auth)
 app.use('/users', usersRoute)
 app.use('/cards', cardsRoute)
 app.get('*', (req, res) => {
   res.status(404).send({ message: 'Page not found' })
 })
-app.post('/signin', login)
-app.post('/signup', createUser)
 
 app.listen(PORT, () => {
   console.log('App is listening to port ', PORT)
